f670eb60783e76de1d5226331bd2c89fb01c96d8
Riaz Laskar <riazcool77@gmail.com>
2019-01-18 03:06:31 +0530

    monor changes

--- index.js
+++ index.js
@@ -5,7 +5,14 @@ const shell = require('shelljs');
 const getos = require('getos');
 const path = require('path');
 const chalk = require('chalk');
+const clear       = require('clear');
 const error = chalk.bold.red;
+
+
+
+
+
+
 // Linux: ~/Android/Sdk
 // Mac: ~/Library/Android/sdk
 // Windows: %LOCALAPPDATA%\Android\sdk
@@ -26,38 +33,50 @@ getos(function(e,data) {
     sdk.push('emulator');
     sdk.push('emulator');
     let sdkpath = path.join.apply(null,sdk);
-     cmd.get(`${sdkpath} -list-avds`,async function(err, data, stderr){
+
+    cmd.get(`${sdkpath} -version`,async function(err, data, stderr){
         if(err) {
             console.log(error(`Could not find android emulator sdk in default location '${sdkpath}'`))
             return;
         }
-        let devices = data.split("\r\n");
 
-        if(!devices.length){
-            console.log(error("No Devices Found !!"))
-            return;
-        }
-    
-        try{
-            const response = await prompt({
-                type: 'select',
-                name: 'avd',
-                message: 'Select a virtual device ?',
-                choices: data.split("\r\n")
-            });
+        clear();
+        console.log(chalk.green(data));
+
+            cmd.get(`${sdkpath} -list-avds`,async function(err, data, stderr){
+
+                let devices = data.split("\r\n");
+        
+                if(!devices.length){
+                    console.log(error("No Devices Found !!"))
+                    return;
+                }
             
-            if(response.avd) {
-                if (shell.exec(`${sdkpath} -avd ${response.avd}`).code !== 0) {
-                    shell.exit(1);
-                  }
-            }
+                try{
+                    const response = await prompt({
+                        type: 'select',
+                        name: 'avd',
+                        message: 'Select a virtual device ?',
+                        choices: data.split("\r\n")
+                    });
+                    
+                    if(response.avd) {
+                        if (shell.exec(`${sdkpath} -avd ${response.avd}`).code !== 0) {
+                            shell.exit(1);
+                        }
+                    }
+            
+                } catch (e) {
+                    console.log(chalk.yellow('Aborted !!'))
+                }
+        
+            
+            })
+    });
     
-        } catch (e) {
-            console.log(chalk.yellow('Aborted !!'))
-        }
+
 
      
-    })
 })
 
 
